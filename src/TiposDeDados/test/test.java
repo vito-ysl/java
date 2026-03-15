9b4dfc3941c409b92870dedabd05013f9aa52def
vito-ysl <vitorevangelista0557@gmail.com>
2026-03-15 02:03:09 -0300

    Primeiros passos

---
+++ src/TiposDeDados/test/test.java
@@ -0,0 +1,14 @@
+package TiposDeDados.test;
+
+import java.time.LocalDateTime;
+import java.time.LocalTime;
+
+public class test {
+    public static void main(String[] args) {
+
+        LocalDateTime data = LocalDateTime.now();
+
+        System.out.printf("Hoje é %s%n", data);
+
+    }
+}
